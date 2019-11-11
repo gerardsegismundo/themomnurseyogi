338bebea027b81aae81de0e661a8cbc1be4c578b
Gerard Segismundo <gerardmartinsegismundo@gmail.com>
2019-11-11 15:58:45 -0800

    updated production asset

--- server/config/productionAsset.js
+++ server/config/productionAsset.js
@@ -5,7 +5,7 @@ const express = require('express')
 module.exports = app => {
   if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('../client/build'))
+    app.use(express.static('../../client/build'))
 
     app.get('*', (req, res) =>
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
