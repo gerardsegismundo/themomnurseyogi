40d65b50a5aa2980911dc6bbd10adb95bfe460ab
Gerard Segismundo <gerardmartinsegismundo@gmail.com>
2019-11-11 15:37:11 -0800

    updated keys

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,6 +1,6 @@
 // Keys for dev and prod
 if (process.env.NODE_ENV === 'production') {
-  return (module.exports = require('./prod.keys'))
+  module.exports = require('./prod.keys')
 } else {
   module.exports = require('./dev.keys')
 }
