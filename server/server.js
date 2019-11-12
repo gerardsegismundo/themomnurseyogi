caf18918c301178aec6988fe0c0f26109af3c12a
Gerard Segismundo <gerardmartinsegismundo@gmail.com>
2019-11-11 22:37:26 -0800

    deploy bitch

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ require('./config/routes')(app)
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../client/build'))
+  app.use(express.static('../client/public'))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../client', 'public', 'index.html'))
   )
 }
 
