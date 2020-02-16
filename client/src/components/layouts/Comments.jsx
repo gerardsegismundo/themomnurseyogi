9afda7b524ecc2af2dbc5bdcc70deebf7626c33d
Gerard Martin R. Segismundo <gerardmartinsegismundo@gmail.com>
2020-02-15 23:24:50 -0800

    adjust comment css

--- client/src/components/layouts/Comments.jsx
+++ client/src/components/layouts/Comments.jsx
@@ -58,82 +58,94 @@ const Comments = ({
   useEscapeClick(isEditingComment, exitEditComment)
 
   return (
-    <div className='comments'>
-      <div className='comments__heading row mb-5'>
-        <h2 className='col'>Comments</h2>
-        <p className='col text-right'>{comments && comments.length} comments</p>
-      </div>
+    <div
+      className={`comments${
+        comments.length >= 1 ? ' mt-6' : ' border-top-none'
+      }`}
+    >
+      {comments.length >= 1 ? (
+        <>
+          <div className='comments__heading row mb-5'>
+            <h2 className='col'>Comments</h2>
+            <p className='col text-right'>
+              {comments && comments.length} comments
+            </p>
+          </div>
 
-      <ul>
-        {comments &&
-          comments.map(item => {
-            const { _id, user, name, avatar, text, date } = item
+          <ul>
+            {comments &&
+              comments.map(item => {
+                const { _id, user, name, avatar, text, date } = item
 
-            return (
-              <li className='comment d-flex' key={_id}>
-                <img
-                  src={avatar}
-                  alt='user'
-                  className='comment__author-avatar'
-                />
+                return (
+                  <li className='comment d-flex' key={_id}>
+                    <img
+                      src={avatar}
+                      alt='user'
+                      className='comment__author-avatar'
+                    />
 
-                <div className='comment__right-col'>
-                  {editedComment._id !== _id && (
-                    <>
-                      <h4 className='comment__label'>
-                        {name} &nbsp;&nbsp;|&nbsp;&nbsp; {formatDate(date)}
-                      </h4>
-
-                      <p className='comment__msg'>{text}</p>
-                    </>
-                  )}
-
-                  {currentUser && user === currentUser.id && (
-                    <div className='comment__crud'>
-                      {!isEditingComment || editedComment._id !== _id ? (
+                    <div className='comment__right-col'>
+                      {editedComment._id !== _id && (
                         <>
-                          <span
-                            className='hidden'
-                            onClick={() => handleEditComment({ text, _id })}
-                          >
-                            edit
-                          </span>
-                          <span
-                            className='hidden'
-                            onClick={() => handleDeleteComment(_id)}
-                          >
-                            delete
-                          </span>
-                        </>
-                      ) : (
-                        <>
-                          <textarea
-                            onChange={e => {
-                              setEditedComment({
-                                _id,
-                                text: e.target.value
-                              })
-                            }}
-                            ref={editedCommentTextarea}
-                            id='edited-comment'
-                            name='edited-comment'
-                            value={editedComment.text}
-                            className='form-control'
-                            placeholder='Write a comment...'
-                            required
-                          ></textarea>
+                          <h4 className='comment__label'>
+                            {name} &nbsp;&nbsp;|&nbsp;&nbsp; {formatDate(date)}
+                          </h4>
 
-                          <span onClick={exitEditComment}>cancel</span>
-                          <span onClick={saveEditComment}>save</span>
+                          <p className='comment__msg'>{text}</p>
                         </>
                       )}
+
+                      {currentUser && user === currentUser.id && (
+                        <div className='comment__crud'>
+                          {!isEditingComment || editedComment._id !== _id ? (
+                            <>
+                              <span
+                                className='hidden'
+                                onClick={() => handleEditComment({ text, _id })}
+                              >
+                                edit
+                              </span>
+                              <span
+                                className='hidden'
+                                onClick={() => handleDeleteComment(_id)}
+                              >
+                                delete
+                              </span>
+                            </>
+                          ) : (
+                            <>
+                              <textarea
+                                onChange={e => {
+                                  setEditedComment({
+                                    _id,
+                                    text: e.target.value
+                                  })
+                                }}
+                                ref={editedCommentTextarea}
+                                id='edited-comment'
+                                name='edited-comment'
+                                value={editedComment.text}
+                                className='form-control'
+                                placeholder='Write a comment...'
+                                required
+                              ></textarea>
+
+                              <span onClick={exitEditComment}>cancel</span>
+                              <span onClick={saveEditComment}>save</span>
+                            </>
+                          )}
+                        </div>
+                      )}
                     </div>
-                  )}
-                </div>
-              </li>
-            )
-          })}
-      </ul>
+                  </li>
+                )
+              })}
+          </ul>
+        </>
+      ) : (
+        <br />
+      )}
 
       <CommentForm />
     </div>
